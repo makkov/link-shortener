4e04e2e2f74f66ce74193a4171a6981c56c59b84
makkov <maxim_86nva@mail.ru>
2024-05-29 21:24:28 +0300

    хранение вынесено в репозиторий, доработаны тесты

--- src/main/java/com/colvir/link/shortener/service/LinkService.java
+++ src/main/java/com/colvir/link/shortener/service/LinkService.java
@@ -5,6 +5,7 @@ import com.colvir.link.shortener.dto.GenerateLinkResponse;
 import com.colvir.link.shortener.exception.LinkNotFoundException;
 import com.colvir.link.shortener.mapper.LinkMapper;
 import com.colvir.link.shortener.model.Link;
+import com.colvir.link.shortener.repository.LinkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.view.RedirectView;
@@ -16,21 +17,21 @@ import java.util.*;
 @RequiredArgsConstructor
 public class LinkService {
 
-    private final Map<String, Link> linkByShortLink = new HashMap<>();
-    private final LinkMapper linkMapper = new LinkMapper();
+    private final LinkMapper linkMapper;
+    private final LinkRepository linkRepository;
 
     public GenerateLinkResponse generateShortLink(GenerateLinkRequest request) {
         String originalLink = request.getOriginalLink();
         String shortLink = Base64.getEncoder().encodeToString(originalLink.getBytes(StandardCharsets.UTF_8));
 
         Link newLink = new Link(originalLink, shortLink);
-        linkByShortLink.put(shortLink, newLink);
+        linkRepository.save(newLink);
 
         return linkMapper.linkToGenerateLinkResponse(newLink);
     }
 
     public RedirectView redirectByShortLink(String shortLink) {
-        Link link = linkByShortLink.get(shortLink);
+        Link link = linkRepository.getByShorted(shortLink);
 
         if (link == null) {
             throw new LinkNotFoundException("Ссылка не найдена");
--- src/test/java/com/colvir/link/shortener/service/LinkServiceTest.java
+++ src/test/java/com/colvir/link/shortener/service/LinkServiceTest.java
@@ -3,31 +3,51 @@ package com.colvir.link.shortener.service;
 import com.colvir.link.shortener.dto.GenerateLinkRequest;
 import com.colvir.link.shortener.dto.GenerateLinkResponse;
 import com.colvir.link.shortener.exception.LinkNotFoundException;
-import org.junit.jupiter.api.Order;
+import com.colvir.link.shortener.mapper.LinkMapper;
+import com.colvir.link.shortener.model.Link;
+import com.colvir.link.shortener.repository.LinkRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.servlet.view.RedirectView;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
-
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        LinkService.class,
+        LinkMapper.class
+})
 class LinkServiceTest {
 
-    LinkService linkService = new LinkService();
+    @Autowired
+    private LinkService linkService;
 
+    @MockBean
+    private LinkRepository linkRepository;
 
     @Test
     void generateShortLink_success() {
         //Подготовка входных данных
-        GenerateLinkRequest request = new GenerateLinkRequest();
+        GenerateLinkRequest request = new GenerateLinkRequest();//todo написать генераторы
         request.setOriginalLink("https://ya.ru");
 
         //Подготовка ожидаемого результата
         GenerateLinkResponse expectedResponse = new GenerateLinkResponse();
         expectedResponse.setShortLink("aHR0cHM6Ly95YS5ydQ==");
 
+        when(linkRepository.save(any())).thenReturn(any());
+
         //Начало теста
         GenerateLinkResponse actualResponse = linkService.generateShortLink(request);
         assertEquals(expectedResponse, actualResponse);
+        verify(linkRepository).save(any());
+        verifyNoMoreInteractions(linkRepository);
     }
 
     @Test
@@ -36,17 +56,16 @@ class LinkServiceTest {
         String shortLink = "aHR0cHM6Ly95YS5ydQ==";
 
         //Подготовка ожидаемого результата
-        GenerateLinkRequest request = new GenerateLinkRequest();
-        request.setOriginalLink("https://ya.ru");
-
-        linkService.generateShortLink(request);
+        Link link = new Link("https://ya.ru", shortLink);
+        when(linkRepository.getByShorted(shortLink)).thenReturn(link);
 
         RedirectView expectedRedirectView = new RedirectView("https://ya.ru");
 
         //Начало теста
         RedirectView actualRedirectView = linkService.redirectByShortLink(shortLink);
-
         assertEquals(expectedRedirectView.getUrl(), actualRedirectView.getUrl());
+        verify(linkRepository).getByShorted(shortLink);
+        verifyNoMoreInteractions(linkRepository);
     }
 
     @Test
@@ -55,10 +74,13 @@ class LinkServiceTest {
         String shortLink = "aHR0cHM6Ly95YS5ydQ==";
 
         //Подготовка ожидаемого результата
+        when(linkRepository.getByShorted(shortLink)).thenReturn(null);
         String expectedMessage = "Ссылка не найдена";
 
         //Начало теста
         Exception exception = assertThrows(LinkNotFoundException.class, () -> linkService.redirectByShortLink(shortLink));
         assertEquals(expectedMessage, exception.getMessage());
+        verify(linkRepository).getByShorted(shortLink);
+        verifyNoMoreInteractions(linkRepository);
     }
 }
