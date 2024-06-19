54e136a26e70198e2594a196eb19a4b1af7d64a4
makkov <maxim_86nva@mail.ru>
2024-06-19 21:36:31 +0300

    к проекту подключено кэширование

--- src/main/java/com/colvir/link/shortener/model/Link.java
+++ src/main/java/com/colvir/link/shortener/model/Link.java
@@ -4,8 +4,6 @@ import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.math.BigDecimal;
-
 @Data
 @Table(name = "links")
 @Entity
--- src/main/java/com/colvir/link/shortener/service/LinkService.java
+++ src/main/java/com/colvir/link/shortener/service/LinkService.java
@@ -4,6 +4,7 @@ import com.colvir.link.shortener.dto.*;
 import com.colvir.link.shortener.exception.LinkNotFoundException;
 import com.colvir.link.shortener.mapper.LinkMapper;
 import com.colvir.link.shortener.model.Link;
+import com.colvir.link.shortener.repository.LinkCacheRepository;
 import com.colvir.link.shortener.repository.LinkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Optional;
 
 import static com.colvir.link.shortener.model.LinkStatus.CREATED;
 
@@ -22,6 +24,7 @@ public class LinkService {
 
     private final LinkMapper linkMapper;
     private final LinkRepository linkRepository;
+    private final LinkCacheRepository linkCacheRepository;
 
     public GenerateLinkResponse generateShortLink(GenerateLinkRequest request) {
         String originalLink = request.getOriginalLink();
@@ -50,8 +53,7 @@ public class LinkService {
     }
 
     public LinkResponse getById(Integer id) {
-        Link link = linkRepository.findById(id)
-                .orElseThrow(() -> new LinkNotFoundException(String.format("Ссылка с id = %s не найдена", id)));
+        Link link = findInCacheOrDbById(id);
         return linkMapper.linkToLinkResponse(link);
     }
 
@@ -74,5 +76,19 @@ public class LinkService {
         linkRepository.deleteById(id);
 
         return linkMapper.linkToLinkResponse(link);
+    }
+
+    private Link findInCacheOrDbById(Integer id) {
+        Optional<Link> linkFromCache = linkCacheRepository.findById(id);
+
+        if (linkFromCache.isPresent()) {
+            return linkFromCache.get();
+        }
+
+        Link linkFromDb = linkRepository.findById(id)
+                .orElseThrow(() -> new LinkNotFoundException(String.format("Ссылка с id = %s не найдена", id)));
+
+        linkCacheRepository.save(linkFromDb);
+        return linkFromDb;
     }
 }
