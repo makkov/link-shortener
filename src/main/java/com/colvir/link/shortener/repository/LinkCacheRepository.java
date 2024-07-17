d802aa02d4350df2547fe26e9eb495733cc47fd3
makkov <maxim_86nva@mail.ru>
2024-07-17 19:54:52 +0300

    добавлен retry на запрос в редис

--- src/main/java/com/colvir/link/shortener/repository/LinkCacheRepository.java
+++ src/main/java/com/colvir/link/shortener/repository/LinkCacheRepository.java
@@ -4,22 +4,30 @@ import com.colvir.link.shortener.model.Link;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Set;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class LinkCacheRepository {
 
     private final RedisTemplate<String, String> redisTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-
+    @Retryable(retryFor = RuntimeException.class, maxAttempts = 10, backoff = @Backoff(delay = 3000L))
     public Optional<Link> findById(Integer id) {
+        System.out.printf("request to Redis, time: %s\n", LocalDateTime.now());
+
         String stringValue = redisTemplate.opsForValue().get(String.valueOf(id));
 
         if (stringValue == null) {
@@ -31,6 +39,12 @@ public class LinkCacheRepository {
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @Recover
+    Optional<Link> handle(RuntimeException e, Integer id) {
+        System.out.printf("Redis is unavailable, Link with id %s will received from DB\n", id);
+        return Optional.empty();
     }
 
     public Link save(Link link) {
